412c4de9d7766178e2a9647a1dde5a6bd219251d
Audric Dubuis <audric.dubuis@heig-vd.ch>
2019-03-27 19:31:46 +0100

    Job done

--- prepareData.js
+++ prepareData.js
@@ -1,16 +1,21 @@
-const fetch = require('node-fetch')
-const d3 = require('d3')
-const R = require('ramda')
+const fetch = require('node-fetch');
+const d3 = require('d3');
+const R = require('ramda');
 
 const writeJson = require('./writeJson')
-/*
-  writeJson prends deux arguments:
-    * le nom du fichier
-    * un objet ou une liste JSON
-  
-  ex:
-    writeJson('asylum.json', data)
-*/
 
-const URL = 'https://raw.githubusercontent.com/idris-maps/heig-datavis-2019/master/20190315-node/exercie_node/ch_asylum_demands.csv'
 // le lien vers le fichier CSV
+const URL = 'https://raw.githubusercontent.com/idris-maps/heig-datavis-2019/master/20190322-node/exercice_node/ch_asylum_demands.csv';
+
+
+fetch(URL).then(res => res.text())
+			.then(d3.csvParse)
+			//Enlève les entrées ou il y a "*" 
+			.then(donnees => donnees.filter(d => d.affected !== "*")) 
+			//Number traduit "1999" en 1999
+			.then(donnees => donnees.map(d => ({...d, affected: Number(d.affected), year: Number(d.year)}))) 
+			//Normalise les USA
+			.then(donnees => donnees.map(d => ({...d, country_asylum: d.country_asylum.includes("USA") ? "USA" : d.country_asylum}) ))
+			//Crée le fichier asylum.json
+			.then(donnees => writeJson('asylum.json', donnees))
+			.catch(console.log);
